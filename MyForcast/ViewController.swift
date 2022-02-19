4be53025bd376f98f42975f0a039ae5a8bf782de
Khalil Maaref MacPro <maaref@mobiblanc.com>
2022-02-19 22:08:37 +0100

    Initial Commit

---
+++ MyForcast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyForcast
+//
+//  Created by Khalil Mobiblanc on 19/2/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
